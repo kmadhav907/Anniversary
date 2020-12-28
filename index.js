221d035890132c1fc9682e8fa676f01f16215c76
kmadhav907 <kmadhav907@gmail.com>
2020-12-28 11:26:04 +0530

    Happy Anniversary

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var ejs = require('ejs')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Server is running')
+})
